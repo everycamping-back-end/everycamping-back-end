1b4fd1a9372aef000021aec3b8d6cafa8461f05d
김세종 <ksj2083@naver.com>
2023-01-03 17:57:07 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/everycampingbackend/EverycampingBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.everycampingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverycampingBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverycampingBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/everycampingbackend/EverycampingBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.everycampingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverycampingBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
