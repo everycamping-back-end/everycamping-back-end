94f3d8d41564b71b97b4d1f0b6caf1d87de116ff
김세종 <ksj2083@naver.com>
2023-01-04 21:38:15 +0900

    feat: [KSJ001-CREATE-ORDER] 로깅 정책 추가
    warning 등급 이상의 로그는 콘솔 및 로그파일에 입력된다. Exception 발생시 stacktrace도 입력하도록 정책 추가

--- src/main/java/com/zerobase/everycampingbackend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/everycampingbackend/common/exception/GlobalExceptionHandler.java
@@ -1,13 +1,12 @@
 package com.zerobase.everycampingbackend.common.exception;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,17 +14,18 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(CustomException.class)
   public ResponseEntity<ExceptionResponse> customRequestException(final CustomException ex) {
-    log.error("api Exception : {} ", ex.getErrorCode());
-    return new ResponseEntity<ExceptionResponse>(
+    log.error("api Exception : {} \nstackTrace : {}", ex.getErrorCode(), ex.getStackTrace());
+    return new ResponseEntity<>(
         new ExceptionResponse(ex.getMessage(), ex.getErrorCode()),
         ex.getErrorCode().getHttpStatus());
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ExceptionResponse> unknownException(final Exception ex) {
-    log.error("unknown Exception : {}, message : {} ", ErrorCode.UNKNOWN_EXCEPTION,
-        ex.getMessage());
-    return new ResponseEntity<ExceptionResponse>(
+    log.error("unknown Exception : {},\nmessage : {} \nstackTrace : {}",
+        ErrorCode.UNKNOWN_EXCEPTION,
+        ex.getMessage(), ex.getStackTrace());
+    return new ResponseEntity<>(
         new ExceptionResponse(ex.getMessage(), ErrorCode.UNKNOWN_EXCEPTION),
         ErrorCode.UNKNOWN_EXCEPTION.getHttpStatus());
   }
