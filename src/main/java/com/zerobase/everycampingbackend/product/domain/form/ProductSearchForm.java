699dba87f032c7d08b4007dddfdf70165e04173c
Syl8n <magi8520@gmail.com>
2023-01-05 23:25:11 +0900

    feat: 고객용 서치 폼 추가
    일부 선 커밋으로 인해 해당 폼이 없어 프로그램이 실행되지 않기 때문에 선 추가를 함.

---
+++ src/main/java/com/zerobase/everycampingbackend/product/domain/form/ProductSearchForm.java
@@ -0,0 +1,19 @@
+package com.zerobase.everycampingbackend.product.domain.form;
+
+import com.zerobase.everycampingbackend.product.type.ProductCategory;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductSearchForm {
+    private String name;
+    private String sellerName;
+    private ProductCategory category;
+    private List<String> tags;
+}
