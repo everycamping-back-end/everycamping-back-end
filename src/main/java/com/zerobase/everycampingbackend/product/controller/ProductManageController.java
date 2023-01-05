d7a189f7b4ee35fdbfabb4982099e11ca829930a
Syl8n <magi8520@gmail.com>
2023-01-05 23:23:42 +0900

    feat: 판매자용 상품 삭제 api 구현
    차후 연관관계 매핑이 발생할 시, 사이드 이펙트 확인 필요.

--- src/main/java/com/zerobase/everycampingbackend/product/controller/ProductManageController.java
+++ src/main/java/com/zerobase/everycampingbackend/product/controller/ProductManageController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,12 @@ public class ProductManageController {
     public ResponseEntity<Boolean> updateProduct(@PathVariable Long productId,
         @RequestBody @Valid ProductManageForm form) {
         productManageService.updateProduct(productId, form);
+        return ResponseEntity.ok(true);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Boolean> deleteProduct(@PathVariable Long productId) {
+        productManageService.deleteProduct(productId);
         return ResponseEntity.ok(true);
     }
 
