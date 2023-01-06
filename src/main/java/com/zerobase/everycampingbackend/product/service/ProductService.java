e3046c46c1662575b57cf41b06c3b728f94dcef7
Syl8n <magi8520@gmail.com>
2023-01-06 23:14:14 +0900

    feat: @Transactional 어노테이션 추가
    검색만 수행하기에 readOnly 값을 true로 적용해 성능 향상.

--- src/main/java/com/zerobase/everycampingbackend/product/service/ProductService.java
+++ src/main/java/com/zerobase/everycampingbackend/product/service/ProductService.java
@@ -11,10 +11,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
     private final ProductRepository productRepository;
 
