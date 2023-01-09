f17b967f0657fcf59b8b2c8e35ab759a273e3f39
김세종 <ksj2083@naver.com>
2023-01-10 00:22:31 +0900

    style: of 패턴 사용

--- src/main/java/com/zerobase/everycampingbackend/cart/service/CartService.java
+++ src/main/java/com/zerobase/everycampingbackend/cart/service/CartService.java
@@ -24,14 +24,12 @@ public class CartService {
     //로그인한 Customer 관련 로직 추가 예정
 
     Product product = productService.getProductById(productId);
-    if(product.getStock() < form.getCount()) {
+    if (product.getStock() < form.getCount()) {
       throw new CustomException(ErrorCode.PRODUCT_NOT_ENOUGH_STOCK);
     }
 
-    cartRepository.save(Cart.builder()
-        .customer(customerService.getCustomerById(form.getCustomerId()))
-        .product(productService.getProductById(productId))
-        .count(form.getCount())
-        .build());
+    cartRepository.save(Cart.of(productService.getProductById(form.getCustomerId()),
+        customerService.getCustomerById((form.getCustomerId())),
+        form.getCount()));
   }
 }
