dc5488447c3cf5cf8206cdc291bb35219753f8da
김세종 <ksj2083@naver.com>
2023-01-11 07:27:53 +0900

    refactor: 장바구니 조회 fetch join 적용
    N+1 문제 방어를 위핸 fetch join 구현

--- src/main/java/com/zerobase/everycampingbackend/cart/domain/repository/CartRepository.java
+++ src/main/java/com/zerobase/everycampingbackend/cart/domain/repository/CartRepository.java
@@ -3,10 +3,14 @@ package com.zerobase.everycampingbackend.cart.domain.repository;
 import com.zerobase.everycampingbackend.cart.domain.entity.CartProduct;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository<CartProduct, Long> {
+
+  @EntityGraph(attributePaths = {"product"}, type = EntityGraphType.LOAD)
   Page<CartProduct> findAllByCustomerId(Long CustomerId, Pageable pageable);
 }
