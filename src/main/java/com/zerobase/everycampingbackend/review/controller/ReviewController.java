70e8c21dc8a79d9a137f77426d25726d3622bce8
Syl8n <magi8520@gmail.com>
2023-01-11 01:53:57 +0900

    feat: Review 조회 기능 구현
    리뷰 상세 조회, 작성자 별 조회, 상품 별 조회 기능 추가.

--- src/main/java/com/zerobase/everycampingbackend/review/controller/ReviewController.java
+++ src/main/java/com/zerobase/everycampingbackend/review/controller/ReviewController.java
@@ -1,17 +1,21 @@
 package com.zerobase.everycampingbackend.review.controller;
 
+import com.zerobase.everycampingbackend.review.domain.dto.ReviewDto;
 import com.zerobase.everycampingbackend.review.domain.form.ReviewForm;
 import com.zerobase.everycampingbackend.review.service.ReviewService;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -42,6 +46,21 @@ public class ReviewController {
     public ResponseEntity<Boolean> deleteReview(Principal principal, @PathVariable Long reviewId) {
         reviewService.deleteReview(principal.getName(), reviewId);
         return ResponseEntity.ok(true);
+    }
+
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<ReviewDto> getReviewDetail(@PathVariable Long reviewId){
+        return ResponseEntity.ok(reviewService.getReviewDetail(reviewId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReviewDto>> getReviewsByCustomerId(@RequestParam Long customerId){
+        return ResponseEntity.ok(reviewService.getReviewsByCustomerId(customerId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReviewDto>> getReviewsByProductId(@RequestParam Long productId){
+        return ResponseEntity.ok(reviewService.getReviewsByProductId(productId));
     }
 
 }
