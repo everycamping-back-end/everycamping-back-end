bd2770e05b9c1de49f5c73b1150bc99ca08ed398
Syl8n <magi8520@gmail.com>
2023-01-11 00:58:15 +0900

    feat: Review 작성 기능 구현
    리뷰 작성은 해당 상품을 구매한 고객만 가능.
    이를 위해 form에 상품Id, 고객Id가 필요.
    첨부된 이미지는 이미지 서버에 업로드 후 경로를 db에 저장한다.

--- src/main/java/com/zerobase/everycampingbackend/review/controller/ReviewController.java
+++ src/main/java/com/zerobase/everycampingbackend/review/controller/ReviewController.java
@@ -1,7 +1,13 @@
 package com.zerobase.everycampingbackend.review.controller;
 
+import com.zerobase.everycampingbackend.review.domain.form.ReviewForm;
 import com.zerobase.everycampingbackend.review.service.ReviewService;
+import java.io.IOException;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,11 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @PostMapping
+    public ResponseEntity<Boolean> writeReview(Principal principal, @RequestBody ReviewForm form)
+        throws IOException {
+        reviewService.writeReview(principal.getName(), form);
+        return ResponseEntity.ok(true);
+    }
 
 }
