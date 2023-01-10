5dc72c7fff2743ada12bb5b03b015ca4cca20ff9
Syl8n <magi8520@gmail.com>
2023-01-11 01:40:58 +0900

    feat: Review 삭제 기능 구현
    리뷰 삭제, 상품 내 리뷰 수 & 총점 변경, 이미지 서버에서 이미지 삭제.

--- src/main/java/com/zerobase/everycampingbackend/review/controller/ReviewController.java
+++ src/main/java/com/zerobase/everycampingbackend/review/controller/ReviewController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -34,6 +35,12 @@ public class ReviewController {
     public ResponseEntity<Boolean> editReview(Principal principal,
         @PathVariable Long reviewId, @RequestBody ReviewForm form) throws IOException {
         reviewService.editReview(principal.getName(), reviewId, form);
+        return ResponseEntity.ok(true);
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<Boolean> deleteReview(Principal principal, @PathVariable Long reviewId) {
+        reviewService.deleteReview(principal.getName(), reviewId);
         return ResponseEntity.ok(true);
     }
 
