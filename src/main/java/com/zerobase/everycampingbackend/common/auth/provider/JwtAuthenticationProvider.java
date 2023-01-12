962e930167e38809db4d0c64b1a7d45fcc61f0df
Syl8n <magi8520@gmail.com>
2023-01-13 02:50:41 +0900

    feat: ErrorCode 추가
    토큰의 유효성에 관한 에러코드 추가.
    
    refactor: Jwt 발급 및 해석에 JwtIssuer 이용, 메소드명 통일, CustomUserDetailsService 인터페이스를 멤버로 활용.
    
    분리했던 JwtIssuer 참조해 사용.
    보통 인증용 메소드명과 재발급 인증용 메소드명을 통일.
    인터페이스 활용으로 if-else 하드코딩 지양.

---
+++ src/main/java/com/zerobase/everycampingbackend/common/auth/provider/JwtAuthenticationProvider.java
@@ -0,0 +1,90 @@
+package com.zerobase.everycampingbackend.common.auth.provider;
+
+
+import com.zerobase.everycampingbackend.common.auth.CustomUserDetailsService;
+import com.zerobase.everycampingbackend.common.auth.issuer.JwtIssuer;
+import com.zerobase.everycampingbackend.common.auth.model.JwtDto;
+import com.zerobase.everycampingbackend.common.auth.model.UserType;
+import com.zerobase.everycampingbackend.common.auth.model.UserVo;
+import com.zerobase.everycampingbackend.common.exception.CustomException;
+import com.zerobase.everycampingbackend.common.exception.ErrorCode;
+import com.zerobase.everycampingbackend.user.service.CustomerService;
+import com.zerobase.everycampingbackend.user.service.SellerService;
+import io.jsonwebtoken.Claims;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationProvider {
+
+    private final CustomerService customerService;
+    private final SellerService sellerService;
+    private final JwtIssuer jwtIssuer;
+    private final Map<String, CustomUserDetailsService> map = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        map.put(UserType.CUSTOMER.name(), customerService);
+        map.put(UserType.SELLER.name(), sellerService);
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public boolean validateToken(String token) {
+        if (!StringUtils.hasText(token)) {
+            return false;
+        }
+        Claims claims = jwtIssuer.getClaims(token);
+        if (claims == null) {
+            return false;
+        }
+        if (claims.getExpiration().before(new Date()) ||
+            ObjectUtils.isEmpty(
+                getUserDetailsService(claims).getRefreshToken(claims.getSubject()))) {
+            throw new CustomException(ErrorCode.TOKEN_NOT_VALID);
+        }
+        return true;
+    }
+
+    public boolean validateToken(JwtDto jwtDto) {
+        if (!StringUtils.hasText(jwtDto.getAccessToken())
+            || !StringUtils.hasText(jwtDto.getRefreshToken())) {
+            return false;
+        }
+
+        Claims accessClaims = jwtIssuer.getClaims(jwtDto.getAccessToken());
+        Claims refreshClaims = jwtIssuer.getClaims(jwtDto.getRefreshToken());
+
+        return accessClaims != null && refreshClaims != null;
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = jwtIssuer.getClaims(token);
+        UserVo userVo = jwtIssuer.getUserVo(claims);
+        UserDetails userDetails = getUserDetailsService(claims).loadUserByUsername(userVo.getEmail());
+
+        return new UsernamePasswordAuthenticationToken(userDetails, userVo,
+            userDetails.getAuthorities());
+    }
+
+    private CustomUserDetailsService getUserDetailsService(Claims claims) {
+        return map.get(claims.get(JwtIssuer.KEY_ROLES, String.class));
+    }
+
+}
