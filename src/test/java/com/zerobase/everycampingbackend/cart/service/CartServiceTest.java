d6260f5db1f1dc15bb07c02d36d0f1a222e95692
김세종 <ksj2083@naver.com>
2023-01-13 16:34:34 +0900

    test: 장바구니 넣기 테스트 트랜잭션 제거

--- src/test/java/com/zerobase/everycampingbackend/cart/service/CartServiceTest.java
+++ src/test/java/com/zerobase/everycampingbackend/cart/service/CartServiceTest.java
@@ -18,6 +18,7 @@ import com.zerobase.everycampingbackend.product.type.ProductCategory;
 import com.zerobase.everycampingbackend.user.domain.entity.Customer;
 import com.zerobase.everycampingbackend.user.domain.repository.CustomerRepository;
 import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -31,221 +32,221 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
-@Transactional
 class CartServiceTest {
 
-  @Autowired
-  CartService cartService;
+    @Autowired
+    CartService cartService;
 
-  @Autowired
-  CustomerRepository customerRepository;
+    @Autowired
+    CustomerRepository customerRepository;
 
-  @Autowired
-  ProductRepository productRepository;
+    @Autowired
+    ProductRepository productRepository;
 
-  @Autowired
-  CartRepository cartRepository;
+    @Autowired
+    CartRepository cartRepository;
 
-  @Autowired
-  ProductService productService;
+    @Autowired
+    ProductService productService;
 
-  @Test
-  @DisplayName("장바구니 넣기 실패 - 재고가 부족해서 실패")
-  @Transactional
-  void createCartProductNotEnoughStock() throws Exception {
+    @AfterEach
+    public void clean() {
+        cartRepository.deleteAll();
+        productRepository.deleteAll();
+    }
 
-    //given
-    Long customerId = createCustomer("ksj2083@naver.com");
-    CreateCartForm createCartForm = CreateCartForm.builder()
-        .customerId(customerId)
-        .quantity(6)
-        .build();
+    @Test
+    @DisplayName("장바구니 넣기 실패 - 재고가 부족해서 실패")
+    void createCartProductNotEnoughStock() throws Exception {
 
-    Long productId = createProduct("잡템", 5, ProductCategory.TENT);
+        //given
+        Long customerId = createCustomer("ksj2083@naver.com");
+        CreateCartForm createCartForm = CreateCartForm.builder()
+            .customerId(customerId)
+            .quantity(6)
+            .build();
 
-    //when
-    CustomException ex = (CustomException)assertThrows(RuntimeException.class, () -> {
-      cartService.createCart(createCartForm, productId);
-    });
+        Long productId = createProduct("잡템", 5, ProductCategory.TENT);
 
-    //then
-    assertEquals(ex.getErrorCode(), ErrorCode.PRODUCT_NOT_ENOUGH_STOCK);
-  }
+        //when
+        CustomException ex = (CustomException) assertThrows(RuntimeException.class, () -> {
+            cartService.createCart(createCartForm, productId);
+        });
 
-  @Test
-  @DisplayName("장바구니 넣기 성공")
-  @Transactional
-  void createCartSuccess() throws Exception {
+        //then
+        assertEquals(ex.getErrorCode(), ErrorCode.PRODUCT_NOT_ENOUGH_STOCK);
+    }
 
-    //given
-    Long customerId = createCustomer("ksj2083@naver.com");
-    CreateCartForm createCartForm = CreateCartForm.builder()
-        .customerId(customerId)
-        .quantity(5)
-        .build();
+    @Test
+    @DisplayName("장바구니 넣기 성공")
+    void createCartSuccess() throws Exception {
 
-    Long productId = createProduct("잡템", 5, ProductCategory.TENT);
+        //given
+        Long customerId = createCustomer("ksj2083@naver.com");
+        CreateCartForm createCartForm = CreateCartForm.builder()
+            .customerId(customerId)
+            .quantity(5)
+            .build();
 
-    //when
-    cartService.createCart(createCartForm, productId);
+        Long productId = createProduct("잡템", 5, ProductCategory.TENT);
 
-    //then
-    PageRequest pageRequest = PageRequest.of(0, 5);
-    Page<CartProduct> result = cartRepository.findAllByCustomerId(customerId, pageRequest);
+        //when
+        cartService.createCart(createCartForm, productId);
 
-    assertEquals(result.getContent().get(0).getQuantity(), 5);
-    assertEquals(result.getContent().get(0).getProduct().getName(), "잡템");
-  }
+        //then
+        PageRequest pageRequest = PageRequest.of(0, 5);
+        Page<CartProduct> result = cartRepository.findAllByCustomerId(customerId, pageRequest);
 
-  @Test
-  @DisplayName("장바구니 조회 성공")
-  @Transactional
-  void getCartProductSuccess() throws Exception {
+        assertEquals(result.getContent().get(0).getQuantity(), 5);
+        assertEquals(result.getContent().get(0).getProduct().getName(), "잡템");
+    }
 
-    //given
-    //유저 세팅
-    Long customerId = createCustomer("ksj2083@naver.com");
+    @Test
+    @DisplayName("장바구니 조회 성공")
+    void getCartProductSuccess() throws Exception {
 
-    //상품 세팅
-    Long productId1 = createProduct("텐트1", 5, ProductCategory.TENT);
-    Long productId2 = createProduct("텐트2", 10, ProductCategory.TENT);
+        //given
+        //유저 세팅
+        Long customerId = createCustomer("ksj2083@naver.com");
 
-    //장바구니에 넣기
-    addToCart(customerId, productId1, 3);
-    addToCart(customerId, productId2, 5);
+        //상품 세팅
+        Long productId1 = createProduct("텐트1", 5, ProductCategory.TENT);
+        Long productId2 = createProduct("텐트2", 10, ProductCategory.TENT);
 
-    //when
+        //장바구니에 넣기
+        addToCart(customerId, productId1, 3);
+        addToCart(customerId, productId2, 5);
 
-    //2번 상품은 재고가 1개 남게 되었음
-    Product product = productService.getProductById(productId2);
-    product.setStock(1);
-    productRepository.save(product);
+        //when
 
-    PageRequest pageRequest = PageRequest.of(0, 5);
-    Page<CartProductDto> cartProductPage = cartService.getCartProductList(customerId, pageRequest);
+        //2번 상품은 재고가 1개 남게 되었음
+        Product product = productService.getProductById(productId2);
+        product.setStock(1);
+        productRepository.save(product);
 
-    //then
-    CartProductDto dto1 = cartProductPage.getContent().get(0);
-    CartProductDto dto2 = cartProductPage.getContent().get(1);
+        PageRequest pageRequest = PageRequest.of(0, 5);
+        Page<CartProductDto> cartProductPage = cartService.getCartProductList(customerId,
+            pageRequest);
 
-    assertEquals(productId1, dto1.getProductId());
-    assertEquals(3, dto1.getQuantity());
-    assertEquals(false, dto1.getIsQuantityChanged());
+        //then
+        CartProductDto dto1 = cartProductPage.getContent().get(0);
+        CartProductDto dto2 = cartProductPage.getContent().get(1);
 
-    assertEquals(productId2, dto2.getProductId());
-    assertEquals(1, dto2.getQuantity());
-    assertEquals(true, dto2.getIsQuantityChanged());
+        assertEquals(productId1, dto1.getProductId());
+        assertEquals(3, dto1.getQuantity());
+        assertEquals(false, dto1.getIsQuantityChanged());
 
-  }
+        assertEquals(productId2, dto2.getProductId());
+        assertEquals(1, dto2.getQuantity());
+        assertEquals(true, dto2.getIsQuantityChanged());
 
-  @Test
-  @DisplayName("장바구니 수량 변경 성공")
-  @Transactional
-  void updateQuantitySuccess() throws Exception {
+    }
 
-    //given
-    //유저 세팅
-    Long customerId = createCustomer("ksj2083@naver.com");
+    @Test
+    @DisplayName("장바구니 수량 변경 성공")
+    void updateQuantitySuccess() throws Exception {
 
-    //상품 세팅
-    Long productId1 = createProduct("텐트1", 5, ProductCategory.TENT);
+        //given
+        //유저 세팅
+        Long customerId = createCustomer("ksj2083@naver.com");
 
-    //장바구니에 넣기
-    addToCart(customerId, productId1, 3);
+        //상품 세팅
+        Long productId1 = createProduct("텐트1", 5, ProductCategory.TENT);
 
-    //when
-    cartService.updateQuantity(customerId,
-        UpdateQuantityForm.builder().
-                      customerId(customerId)
-                      .updateQuantity(4)
-                      .build());
+        //장바구니에 넣기
+        addToCart(customerId, productId1, 3);
 
-    //then
-    CartProduct cartProduct = cartRepository.findByCustomerIdAndProductId(
-        customerId, productId1).orElseThrow();
+        //when
+        cartService.updateQuantity(customerId,
+            UpdateQuantityForm.builder().
+                customerId(customerId)
+                .updateQuantity(4)
+                .build());
 
-    assertEquals(4, cartProduct.getQuantity());
-  }
+        //then
+        CartProduct cartProduct = cartRepository.findByCustomerIdAndProductId(
+            customerId, productId1).orElseThrow();
 
-  @Test
-  @DisplayName("장바구니 수량 변경 실패 - 재고가 부족해서 실패")
-  @Transactional
-  void updateQuantityNotEnoughStock() throws Exception {
+        assertEquals(4, cartProduct.getQuantity());
+    }
 
-    //given
-    //유저 세팅
-    Long customerId = createCustomer("ksj2083@naver.com");
+    @Test
+    @DisplayName("장바구니 수량 변경 실패 - 재고가 부족해서 실패")
+    void updateQuantityNotEnoughStock() throws Exception {
 
-    //상품 세팅
-    Long productId1 = createProduct("텐트1", 5, ProductCategory.TENT);
+        //given
+        //유저 세팅
+        Long customerId = createCustomer("ksj2083@naver.com");
 
-    //장바구니에 넣기
-    addToCart(customerId, productId1, 3);
+        //상품 세팅
+        Long productId1 = createProduct("텐트1", 5, ProductCategory.TENT);
 
-    //when
-    CustomException ex = (CustomException) assertThrows(RuntimeException.class, () -> {
-      cartService.updateQuantity(customerId,
-          UpdateQuantityForm.builder().
-              customerId(customerId)
-              .updateQuantity(6)
-              .build());
-    });
+        //장바구니에 넣기
+        addToCart(customerId, productId1, 3);
 
-    //then
-    assertEquals(ErrorCode.PRODUCT_NOT_ENOUGH_STOCK, ex.getErrorCode());
-  }
+        //when
+        CustomException ex = (CustomException) assertThrows(RuntimeException.class, () -> {
+            cartService.updateQuantity(productId1,
+                UpdateQuantityForm.builder().
+                    customerId(customerId)
+                    .updateQuantity(6)
+                    .build());
+        });
 
-  @Test
-  @DisplayName("장바구니 물품 삭제 성공")
-  @Transactional
-  void deleteCartProductSuccess() throws Exception {
+        //then
+        assertEquals(ErrorCode.PRODUCT_NOT_ENOUGH_STOCK, ex.getErrorCode());
+    }
 
-    //given
-    //유저 세팅
-    Long customerId = createCustomer("ksj2083@naver.com");
+    @Test
+    @DisplayName("장바구니 물품 삭제 성공")
+    void deleteCartProductSuccess() throws Exception {
 
-    //상품 세팅
-    Long productId1 = createProduct("텐트1", 5, ProductCategory.TENT);
+        //given
+        //유저 세팅
+        Long customerId = createCustomer("ksj2083@naver.com");
 
-    //장바구니에 넣기
-    addToCart(customerId, productId1, 3);
+        //상품 세팅
+        Long productId1 = createProduct("텐트1", 5, ProductCategory.TENT);
 
-    //when
-    cartService.deleteCartProduct(productId1, customerId);
+        //장바구니에 넣기
+        addToCart(customerId, productId1, 3);
 
-    //then
-    Optional<CartProduct> optionalCartProduct = cartRepository.findByCustomerIdAndProductId(
-        customerId, productId1);
+        //when
+        cartService.deleteCartProduct(productId1, customerId);
 
-    assertFalse(optionalCartProduct.isPresent());
-  }
+        //then
+        Optional<CartProduct> optionalCartProduct = cartRepository.findByCustomerIdAndProductId(
+            customerId, productId1);
 
-  private void addToCart(Long customerId, Long productId, Integer quantity) {
-    cartService.createCart(CreateCartForm.builder()
-        .customerId(customerId)
-        .quantity(quantity)
-        .build(), productId);
-  }
+        assertFalse(optionalCartProduct.isPresent());
+    }
+
+    private void addToCart(Long customerId, Long productId, Integer quantity) {
+        cartService.createCart(CreateCartForm.builder()
+            .customerId(customerId)
+            .quantity(quantity)
+            .build(), productId);
+    }
 
 
-  private Long createProduct(String name, int stock, ProductCategory category) {
-    Product product = Product.builder()
-        .name(name)
-        .category(category)
-        .stock(stock)
-        .build();
+    private Long createProduct(String name, int stock, ProductCategory category) {
+        Product product = Product.builder()
+            .name(name)
+            .category(category)
+            .stock(stock)
+            .build();
 
-    Product saved = productRepository.save(product);
-    return saved.getId();
-  }
+        Product saved = productRepository.save(product);
+        return saved.getId();
+    }
 
-  private Long createCustomer(String email){
-    Customer customer = Customer.builder()
-        .email(email)
-        .build();
+    private Long createCustomer(String email) {
+        Customer customer = Customer.builder()
+            .email(email)
+            .build();
 
-    Customer saved = customerRepository.save(customer);
-    return saved.getId();
-  }
+        Customer saved = customerRepository.save(customer);
+        return saved.getId();
+    }
 
 }
