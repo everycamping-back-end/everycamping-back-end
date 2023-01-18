58e0f92edf3fb8ef651e8a41a0cf23e9a785f98e
김세종 <ksj2083@naver.com>
2023-01-19 05:10:17 +0900

    refactor: 시큐리티 필터 설정 수정
    로그인이 필요없는 기능 필터에서 허용하도록 변경

--- src/main/java/com/zerobase/everycampingbackend/config/SecurityConfiguration.java
+++ src/main/java/com/zerobase/everycampingbackend/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.zerobase.everycampingbackend.domain.auth.filter.JwtAuthFilter;
 import com.zerobase.everycampingbackend.domain.auth.model.UserType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,13 +28,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final String[] AUTH_WHITELIST = {
         "/"
+        //계정 관련
         , "/customers/signin"
         , "/customers/signup"
         , "/customers/reissue"
         , "/sellers/signin"
         , "/sellers/signup"
         , "/sellers/reissue"
+
+        //테스트
         , "/test/**"
+    };
+
+    private static final String[] AUTH_ALLOW_GET_LIST = {
+        // 비회원 조회 가능한 요소들
+        "/products/**" //상품 조회
+        , "/reviews/**" //리뷰 조회
     };
 
     @Override
@@ -50,6 +60,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .authorizeRequests()
             .antMatchers(AUTH_WHITELIST).permitAll()
+            .antMatchers(HttpMethod.GET, AUTH_ALLOW_GET_LIST).permitAll()
             .antMatchers("/admin/**").hasRole(UserType.ADMIN.name())
             .antMatchers("/sellers/**").hasRole(UserType.SELLER.name())
             .anyRequest().hasAnyRole(UserType.CUSTOMER.name(), UserType.SELLER.name(), UserType.ADMIN.name())
