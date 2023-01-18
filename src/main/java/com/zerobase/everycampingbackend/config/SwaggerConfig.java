8eb3d0609034899ecf6e5083e3859c0596300b71
김세종 <ksj2083@naver.com>
2023-01-19 04:56:08 +0900

    feat: swagger 의존성 추가 및 기본 설정

---
+++ src/main/java/com/zerobase/everycampingbackend/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.zerobase.everycampingbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.zerobase.everycampingbackend"))
+            .paths(PathSelectors.any())
+            .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("EveryCamping api 명세(개발용)")
+            .description("개발용 스웨거 입니다.")
+            .version("1.0")
+            .build();
+    }
+}
