d2c110f9aa36b0cedc47a06611d0694be0a2db79
Syl8n <magi8520@gmail.com>
2023-01-19 23:18:12 +0900

    feat: 인증코드용 폼 어노테이션 추가
    Setter, 기본 생성자 추가

--- src/main/java/com/zerobase/everycampingbackend/common/authcode/AuthCodeForm.java
+++ src/main/java/com/zerobase/everycampingbackend/common/authcode/AuthCodeForm.java
@@ -3,8 +3,12 @@ package com.zerobase.everycampingbackend.common.authcode;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthCodeForm {
     @NotBlank
