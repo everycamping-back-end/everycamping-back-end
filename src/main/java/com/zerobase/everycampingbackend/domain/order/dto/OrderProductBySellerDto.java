631bec321afca1e97b59a0f657ffcd8df735e7ee
김세종 <ksj2083@naver.com>
2023-01-20 11:53:50 +0900

    refactor: 판매자 주문 조회 반환값 수정

--- src/main/java/com/zerobase/everycampingbackend/domain/order/dto/OrderProductBySellerDto.java
+++ src/main/java/com/zerobase/everycampingbackend/domain/order/dto/OrderProductBySellerDto.java
@@ -1,6 +1,7 @@
 package com.zerobase.everycampingbackend.domain.order.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zerobase.everycampingbackend.domain.order.type.OrderStatus;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -16,20 +17,25 @@ import lombok.ToString;
 @AllArgsConstructor
 public class OrderProductBySellerDto {
 
+    //스냅샷
+    private String productNameSnapshot; // 주문 시 상품명
+    private Integer stockPriceSnapshot; // 주문 시 개당 가격
+    private String imageUriSnapshot; // 주문 시 상품 이미지 url
+
     //상품 관련정보
     private Long productId;
-    private String productName;
-    private Integer stockPrice;
-    private String imagePath;
 
     //주문 관련정보
     private Long orderProductId;
-    private Integer quantity;
-    private Integer amount;
-    private OrderStatus status;
-    private LocalDateTime createdAt;
+    private String address; //배송지 주소
+    private String phone; //수령인 전화번호
+    private Integer quantity; //주문수량
+    private Integer amount; // 총 금액(구매자가 지불한 금액)
+    private OrderStatus status; //주문 상태
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt; // 주문 일자
 
     //구매자 관련 정보
     private Long customerId;
-    private String customerNickName;
 }
