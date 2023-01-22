720a475134e97797b6a1532fc208cb40db93f877
Syl8n <magi8520@gmail.com>
2023-01-23 04:42:42 +0900

    refactor: Errorcode Httpstatus 변경
    찾을 수 없는 것 -> NOT_FOUND
    권한 부족 -> UNAUTHORIZED

--- src/main/java/com/zerobase/everycampingbackend/exception/ErrorCode.java
+++ src/main/java/com/zerobase/everycampingbackend/exception/ErrorCode.java
@@ -20,20 +20,20 @@ public enum ErrorCode {
   ORDER_CHANGE_STATUS_NOT_AUTHORISED(HttpStatus.UNAUTHORIZED, "확정/취소 권한이 없습니다."),
 
 
-  LOGIN_CHECK_FAIL(HttpStatus.BAD_REQUEST, "일치하는 정보가 없습니다."),
-  USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 회원입니다."),
+  LOGIN_CHECK_FAIL(HttpStatus.NOT_FOUND, "일치하는 정보가 없습니다."),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
   EMAIL_BEING_USED(HttpStatus.BAD_REQUEST, "사용중인 이메일입니다."),
   USER_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED, "수정 권한이 없습니다."),
 
 
-  PRODUCT_NOT_FOUND(HttpStatus.BAD_REQUEST, "상품을 찾을 수 없습니다."),
-  PRODUCT_SELLER_NOT_MATCHED(HttpStatus.BAD_REQUEST, "상품 수정/삭제 권한이 없습니다."),
+  PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
+  PRODUCT_SELLER_NOT_MATCHED(HttpStatus.UNAUTHORIZED, "상품 수정/삭제 권한이 없습니다."),
   PRODUCT_NOT_ENOUGH_STOCK(HttpStatus.BAD_REQUEST, "상품의 재고가 부족합니다."),
   PRODUCT_NOT_ON_SALE(HttpStatus.BAD_REQUEST, "상품이 판매 중이 아닙니다."),
 
-  REVIEW_NOT_FOUND(HttpStatus.BAD_REQUEST, "리뷰를 찾을 수 없습니다."),
+  REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "리뷰를 찾을 수 없습니다."),
   REVIEW_WRITER_NOT_QUALIFIED(HttpStatus.BAD_REQUEST, "리뷰 작성은 해당 상품을 구매한 고객만 가능합니다."),
-  REVIEW_EDITOR_NOT_MATCHED(HttpStatus.BAD_REQUEST, "리뷰 수정/삭제 권한이 없습니다."),
+  REVIEW_EDITOR_NOT_MATCHED(HttpStatus.UNAUTHORIZED, "리뷰 수정/삭제 권한이 없습니다."),
 
   TOKEN_NOT_VALID(HttpStatus.FORBIDDEN, "유효하지 않은 토큰입니다."),
   TOKEN_STILL_ALIVE(HttpStatus.BAD_REQUEST, "재발급 대상 토큰이 아닙니다."),
