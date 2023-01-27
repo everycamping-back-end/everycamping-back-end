764697681bd0fe4d52afbedefc7a9e0feb62d912
Syl8n <magi8520@gmail.com>
2023-01-27 19:08:11 +0900

    refactor: Security Config debug mode 해제
    debug mode 해제

--- src/main/java/com/zerobase/everycampingbackend/config/SecurityConfiguration.java
+++ src/main/java/com/zerobase/everycampingbackend/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
