307bd36618b87119a3ee217a0e66b61739d8e908
김세종 <ksj2083@naver.com>
2023-01-27 16:52:00 +0900

    feat: 조회 시 날짜 포멧 보기 쉽게 변경

--- src/main/java/com/zerobase/everycampingbackend/domain/settlement/dto/DailySettlementDto.java
+++ src/main/java/com/zerobase/everycampingbackend/domain/settlement/dto/DailySettlementDto.java
@@ -1,6 +1,7 @@
 package com.zerobase.everycampingbackend.domain.settlement.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zerobase.everycampingbackend.domain.settlement.entity.DailySettlement;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -19,7 +20,11 @@ public class DailySettlementDto {
 
     private Long id;
     private Long amount; //총액
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate targetDate; //정산대상일
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt; //정산처리일
 
     public static DailySettlementDto from(DailySettlement dailySettlement) {
