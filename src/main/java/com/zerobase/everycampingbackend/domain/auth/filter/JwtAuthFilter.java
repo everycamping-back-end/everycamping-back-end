eeefd483161ad61f2768048a217650a457935966
Bibidi <byungdeuk.kim@gmail.com>
2023-01-29 22:31:23 +0900

    refactor : 잘못 들어간 코드 삭제

--- src/main/java/com/zerobase/everycampingbackend/domain/auth/filter/JwtAuthFilter.java
+++ src/main/java/com/zerobase/everycampingbackend/domain/auth/filter/JwtAuthFilter.java
@@ -25,7 +25,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
 
-        System.out.println(request.getRequestURL().toString());
         boolean isWebSocketHandShakingRequest = request.getRequestURL().toString().contains("websocket");
         if (isWebSocketHandShakingRequest) {
             filterChain.doFilter(request, response);
