18423a367392a039ebf2673cb5416735296b13e2
김세종 <ksj2083@naver.com>
2023-01-31 21:23:21 +0900

    chore: 주석 작성

--- src/main/java/com/zerobase/everycampingbackend/config/JasyptConfig.java
+++ src/main/java/com/zerobase/everycampingbackend/config/JasyptConfig.java
@@ -6,6 +6,14 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * DB 비밀번호 등 비밀키를 암호화 하기 위한 config 입니다.
+ * 아래 사이트에서 암호화, 복호화 가능합니다.
+ * https://www.devglan.com/online-tools/jasypt-online-encryption-decryption
+ *
+ * 복호화 하기위해서는 JASYPT_SECRET_KEY가 필요하며, 환경변수로 입력해 줘야 합니다.
+ * 따라서 JASYPT_SECRET_KEY는 별도 채널로 관리하여야 합니다.
+ */
 @Configuration
 public class JasyptConfig {
 
@@ -14,7 +22,7 @@ public class JasyptConfig {
 
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(System.getenv("JASYPT_PASSWORD"));
+        config.setPassword(System.getenv("JASYPT_SECRET_KEY"));
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
